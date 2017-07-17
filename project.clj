14416dfe8e6d736de4f9b783e4bf1236df075aa6
emiruz <emir@usgroupltd.uk>
2017-07-17 11:39:28 +0100

    Refactoring for documentation

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject dataset-tools "0.1.0-SNAPSHOT"
+(defproject dataset-tools "0.1.0"
   :description "Easy to use library for working with core.matrix datasets in Clojure."
   :url "https://github.com/emiruz/dataset-tools"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.mikera/core.matrix "0.60.3"]])
+                 [net.mikera/core.matrix "0.60.3"]]
+  :plugins [[lein-codox "0.10.3"]])
--- src/dataset_tools/core.clj
+++ src/dataset_tools/core.clj
@@ -1,18 +1,22 @@
 (ns dataset-tools.core
+  "A versatile set of functions for working with core.matrix.dataset
+   datasets. Please see the examples for usage."
   (:require [clojure.string :as str]
             [clojure.core.matrix.dataset :as md]
             [clojure.core.matrix :as m]
             [clojure.set :as cset]
             [clojure.core.reducers :as red]))
 
-(defn from-dataset[ds]
+(defn from-dataset
   "Returns a lazy sequence of maps from a dataset ds."
+  [ds]
   (let [cols (md/column-names ds)]
     (map #(zipmap cols %) ds)))
 
-(defn to-dataset[ks coll]
+(defn to-dataset
   "Converts a map sequence from coll to a dataset using only
    the keys specified in ks."
+  [ks coll]
   (md/dataset
      ks
    (m/matrix
@@ -22,38 +26,42 @@
                #(select-keys % ks))
               coll)))))
 
-(defn select[cols ds]
+(defn select
   "Returns the dataset ds only containing columns specified in cols, 
    or returns a column vector if a single column is specified in cols."
+  [cols ds]
   (let [cond (or (seq? cols) (vector? cols))
         c (if cond cols (conj [] cols))
         n (md/select-columns ds c)]
     (if cond n (m/get-column n 0))))
 
-(defn where[pred ds]
+(defn where
   "Returns the dataset ds filtered on predicate pred. Pred is expected
    to be a unitary function which expects a map representation of each
    row as an input and returns true/false."
+  [pred ds]
   (->> ds
        from-dataset
        (clojure.core/filter pred)
        (to-dataset (md/column-names ds))))
 
-(defn order[cols ds]
+(defn order
   "Returns the dataset ds sorted in ascending order on the columns
    specified in cols."
+  [cols ds]
   (->> ds
        from-dataset
        (sort-by (apply juxt (flatten [cols])))
        (to-dataset (md/column-names ds))))
 
-(defn aggregate[cols f ds]
+(defn aggregate
   "Returns dataset ds grouped by cols (list/vector), by
    function (or map of functions) f, a grouped aggregate.
    f can both be a function, or a map of functions, in which
    case the maps column names will be used as fields. Note that
    the function f is uniary and should expect a vector as input, and
    it should return a scalar as output."
+  [cols f ds]
   (let [c (flatten [cols])
         k (if (map? f) (keys f) '(:group-value))
         v (if (map? f) (vals f) [f])
@@ -67,10 +75,11 @@
          (map #(apply merge %))
          (to-dataset (concat c k)))))
 
-(defn join[fx fy dsx dsy & {:keys [left] :or {:left false}}]
+(defn join
   "Join two datasets (dsx, dsy) using the key functions fx and fy.
    If :left is set to true then the join will be a left join, otherwise
    the join will be an inner join."
+  [fx fy dsx dsy & {:keys [left] :or {:left false}}]
   (let [m (->> dsy
                from-dataset
                (group-by fy))
@@ -90,12 +99,22 @@
          flatten
          (to-dataset ucols))))
 
-(defn cross-tab[cols col f ds]
+(defn cross-tab
   "Returns a cross-tabulation of dataset ds with the columns in cols
    forming the rows. The unique values of the column in col are used to
    form the columns. The intersection of rows and columns is calculated by
    passing the vector of values to f which is expected to return a scalar."
-  (->> ds
-       from-dataset
-       (group-by #(select-keys % (conj (flatten [cols]) col)))
-  ))
+  [cols col f ds]
+  (let [d (distinct (select col ds))
+        xcols (flatten [cols])]
+        (->> ds
+             from-dataset
+             (group-by #(select-keys % xcols))
+             (map
+              (fn[x]
+                (let [g (into {} (group-by #(get % col) (peek x)))
+                      m (first x)]
+                  (reduce
+                   (fn[y z] (update y z (constantly (f (get g z))))) m d))))
+             (to-dataset (concat xcols d))
+             )))
